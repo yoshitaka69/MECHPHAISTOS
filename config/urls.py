a3b4907df5af8df744ecadb362279053313efc4f
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-01-07 08:34:35 +0900

    Delete config directory

--- config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', include('ce.urls')),
-    path('csvinout/', include('csvinout.urls')),
-    path('api/v1/', include('exdisplay.urls')),#exdisplay
-]
