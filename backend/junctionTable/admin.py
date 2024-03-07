870d3fb0352b59b74e9fd5e22684bf3fef5e16e6
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-03-08 07:54:43 +0900

    Update admin.py

--- backend/junctionTable/admin.py
+++ backend/junctionTable/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+class JunctionTableAdmin(admin.ModelAdmin):
+    list_display = ('taskCode','taskCost', 'bomCode', 'bomCost','totalCost')
+    search_fields = ('taskCode','taskCost', 'bomCode', 'bomCost',)
+    list_filter = ('taskCode',) # adminで右側にあるフィルターBOXのこと
+    ordering = ('taskCode',) # 表示する順番
+    save_on_top = True #上部にもsaveボタンを配置
+    #readonly_fields = ("title", )  あとでreadonlyのやつだとかを入れる予定。20240305 y.noto
+
+    list_per_page = 50 # １ページあたりに表示するオブジェクト数を指定
+# 以下でadminサイトに表示させる
+admin.site.register(CeList,JunctionTableAdmin)
