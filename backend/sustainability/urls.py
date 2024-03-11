3d913d76539b8e056366719087bfcbd7fe5b168d
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-03-12 08:18:27 +0900

    Update urls.py

--- backend/sustainability/urls.py
+++ backend/sustainability/urls.py
@@ -4,15 +4,21 @@ from .views import Co2ViewSet, CompanyCodeCo2ViewSet, StmViewSet, ElectricityUsa
 
 router = DefaultRouter()
 router.register(r'co2', Co2ViewSet, basename='co2')
-router.register(r'company-code-co2', CompanyCodeCo2ViewSet, basename='company-code-co2')
+router.register(r'companyCode-co2', CompanyCodeCo2ViewSet, basename='companyCode-co2')
 router.register(r'stm', StmViewSet, basename='stm')
+router.register(r'companyCode-stm', CompanyCodeStmViewSet, basename='companyCode-stm')
 router.register(r'electricityUsage', ElectricityUsageViewSet, basename='electricityUsage')
+router.register(r'companyCode-elec', CompanyCodeElectricityUsageViewSet, basename='companyCode-elec')
 router.register(r'compressedAir', CompressedAirViewSet, basename='compressedAir')
+router.register(r'companyCode-compAir', CompanyCodeCompressedViewSet, basename='companyCode-compAir')
 router.register(r'wellWater', WellWaterViewSet, basename='wellWater')
+router.register(r'companyCode-wellWater', CompanyCodeWellWaterViewSet, basename='companyCode-wellWater')
 router.register(r'pureWater', PureWaterViewSet, basename='pureWater')
+router.register(r'companyCode-pureWater', CompanyCodePureWaterViewSet, basename='companyCode-pureWater')
 router.register(r'wwt', WwtViewSet, basename='wwt')
+router.register(r'companyCode-wwt', CompanyCodeWwtViewSet, basename='companyCode-wwt')
 router.register(r'exhaustGas', ExhaustGasViewSet, basename='exhaustGas')
-
+router.register(r'companyCode-exhaustGas', CompanyCodeExhaustGasViewSet, basename='companyCode-exhaustGas')
 
 urlpatterns = [
     path('sustainability/', include(router.urls)),
