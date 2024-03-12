d610874156c347fd25f95a2905e983eb7f6e2285
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-03-12 10:15:20 +0900

    Update views.py

--- backend/nearMiss/views.py
+++ backend/nearMiss/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 
 from .models import NearMiss, CompanyCode
 from .serializers import NearMissSerializer, CompanyNearMissSerializer
@@ -10,18 +9,9 @@ class NearMissViewSet(viewsets.ModelViewSet):
     queryset = NearMiss.objects.all()
     serializer_class = NearMissSerializer
 
-    # list と retrieve メソッドは ModelViewSet によって提供されるため、
-    # ここではオーバーライドする必要はありません。
-
 class NearMissByCompanyViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = NearMissSerializer
 
     def get_queryset(self):
         companyCode_slug = self.kwargs['companyCode_slug']
-        return NearMiss.objects.filter(companyCode__slug=companyCode_slug)#このslugは設定してないからいつか変更が必要
-
-@api_view(['GET'])
-def company_near_miss_list(request):
-    companies = CompanyCode.objects.all()
-    serializer = CompanyNearMissSerializer(companies, many=True)
-    return Response(serializer.data)
+        return CompanyCode.objects.prefetch_related('nearMiss_companyCode').all()
