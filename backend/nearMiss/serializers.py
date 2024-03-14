b65cf0e413b31b8260a9e8c1458fbd6fde1e43c6
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-03-15 08:52:30 +0900

    Update serializers.py

--- backend/nearMiss/serializers.py
+++ backend/nearMiss/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import NearMiss, CompanyCode
+from .models import NearMiss, CompanyCode,SafetyIndicators,
+from django.db.models import Count, Q
 
 class NearMissSerializer(serializers.ModelSerializer):
     nearMissNo = serializers.CharField(read_only=True)
@@ -59,3 +60,48 @@ class CompanyNearMissSerializer(serializers.ModelSerializer):
     class Meta:
         model = CompanyCode
         fields = ['companyCode', 'nearMissList']
+
+
+
+class SafetyIndicatorsSerializer(serializers.ModelSerializer):
+    safetyIndicators = serializers.SerializerMethodField()
+    totalOfNearMiss = serializers.SerializerMethodField()
+    rateOflevelA = serializers.SerializerMethodField()
+    rateOfActionItems = serializers.SerializerMethodField()
+
+    class Meta:
+        model = SafetyIndicators
+        fields = ['companyCode', 'companyName', 'safetyIndicators', 'totalOfNearMiss', 'rateOflevelA', 'ActionItems', 'solvedActionItems', 'rateOfActionItems']
+
+    def get_total_of_near_miss(self, obj):
+        return NearMiss.objects.filter(companyCode=obj.companyCode).count()
+
+    def get_rate_of_level_a(self, obj):
+        total_near_miss = NearMiss.objects.filter(companyCode=obj.companyCode).count()
+        if total_near_miss > 0:
+            level_a_count = NearMiss.objects.filter(companyCode=obj.companyCode, measures__contains='A').count()
+            return (level_a_count / total_near_miss) * 100
+        else:
+            return 0
+
+    def get_rate_of_action_items(self, obj):
+        total_action_items = obj.ActionItems.count()
+        solved_action_items = obj.solvedActionItems.count()
+        if total_action_items > 0:
+            return (solved_action_items / total_action_items) * 100
+        else:
+            return 0
+
+    def get_safety_indicators(self, obj):
+        rate_of_level_a = self.get_rate_of_level_a(obj)
+        rate_of_action_items = self.get_rate_of_action_items(obj)
+        if rate_of_level_a >= 40:
+            return 'High'
+        elif rate_of_level_a < 40:
+            if rate_of_action_items > 80:
+                return 'High'
+            elif 40 < rate_of_action_items <= 80:
+                return 'Middle'
+            elif 0 < rate_of_action_items <= 40:
+                return 'Low'
+        return 'Undefined'
