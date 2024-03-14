458941dd3d5e741edf711df52d36e2b5efc76446
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-03-15 08:56:00 +0900

    Update models.py

--- backend/nearMiss/models.py
+++ backend/nearMiss/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from accounts.models import CompanyCode,CompanyName,CustomUser
 
+
 class NearMiss(models.Model):
 
     #on_delateはいちよPROTECTにしておく。ビッグデータは財産として残したいがプライバシーポリシーとも相談になる。
@@ -35,12 +36,25 @@ class NearMiss(models.Model):
     def __str__(self):
             return f'{self.nearMissNo}'
 
-class NeededActionItemList(models.Model):
+
+
+class ActionItemList(models.Model):
     companyCode = models.ForeignKey(CompanyCode, on_delete=models.CASCADE, related_name='safetyindicators_companyCode',null=True, blank=True)
     companyName = models.ForeignKey(CompanyName, on_delete=models.CASCADE, related_name='safetyindicators_companyName', null=True, blank=True)
     actionItems = models.IntegerField(verbose_name='actionItems',null=True,blank=True,default=0)
     solvedActionItems = models.IntegerField(verbose_name='solvedActionItems',null=True,blank=True,default=0)
+
     
+    class Meta:
+        verbose_name = 'Action Item List'
+        verbose_name_plural = 'Action Item List'
+        ordering = ('-companyCode',)
+
+    def __str__(self):
+            return f'{self.companyCode}'
+
+
+
 
 class SafetyIndicators(models.Model):
     
@@ -52,55 +66,8 @@ class SafetyIndicators(models.Model):
     ActionItems = models.ForeignKey(CompanyCode, on_delete=models.CASCADE, related_name='safetyindicators_actionItems',null=True, blank=True)
     solvedActionItems = models.ForeignKey(CompanyCode, on_delete=models.CASCADE, related_name='safetyindicators_solvedActionItems',null=True, blank=True)
     rateOfActionItems = models.IntegerField(verbose_name='totalOfNearMiss',null=True,blank=True,default=0)
-    
-    
-    def update_total_of_near_miss(self):
-        # NearMiss モデルのレコード数をカウント
-        total = NearMiss.objects.filter(companyCode=self.companyCode).count()
-        self.totalOfNearMiss = total
-        self.save()
 
-        #MeasuresのAの値をカウントする。
-        update_rate_of_level_a(self):
-        total_near_miss = NearMiss.objects.filter(companyCode=self.companyCode).count()
-        if total_near_miss > 0:
-            level_a_count = NearMiss.objects.filter(
-                companyCode=self.companyCode,
-                measures__contains='A'  # 'measures' フィールドで 'A' を含むレコードをフィルタリング
-            ).count()
-            self.rateOflevelA = (level_a_count / total_near_miss) * 100
-        else:
-            self.rateOflevelA = 0  # totalOfNearMiss が 0 の場合はレートも 0 とする
-        self.save()
-
-        #rateOfactionItemsの関数
-        update_rate_of_action_items(self):
-        total_action_items = ActionItems.objects.filter(companyCode=self.companyCode).count()
-        solved_action_items = solvedActionItems.objects.filter(companyCode=self.companyCode).count()
-
-        if total_action_items > 0:
-            rate = (solved_action_items / total_action_items) * 100
-            self.rateOfActionItems = round(rate, 2)  # 結果を四捨五入して2桁の小数点以下まで表示
-        else:
-            self.rateOfActionItems = 0  # トータルが0の場合は率も0とする
-
-        self.save()
-
-        #safetyindicatorsの指標
-        get_safety_level(self):
-            if self.rateOflevelA >= 40:
-                return 'High'
-            elif self.rateOflevelA < 40:
-                if self.rateOfActionItems > 80:
-                    return 'High'
-                elif 40 < self.rateOfActionItems <= 80:
-                    return 'Middle'
-                elif 0 < self.rateOfActionItems <= 40:
-                    return 'Low'
-            return 'Undefined'
-
-
-    
+   
     class Meta:
         verbose_name = 'Safety Indicators'
         verbose_name_plural = 'Safety Indicators'
