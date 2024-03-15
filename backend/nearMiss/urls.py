00631d40f93e5f789984804edef5895e93713421
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-03-15 10:01:34 +0900

    Update urls.py

--- backend/nearMiss/urls.py
+++ backend/nearMiss/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import NearMissViewSet, CompanyNearMissViewSet
+from .views import NearMissViewSet, CompanyNearMissViewSet, ActionItemListViewSet, SafetyIndicators
 
 # ルーターの設定
 router = DefaultRouter()
 router.register(r'nearMiss', NearMissViewSet, basename='nearMiss')
 router.register(r'companyCode-nearMiss', CompanyNearMissViewSet, basename='companyCode-nearMiss')
+router.register(r'actionItemsList', ActionItemListViewSet, basename='actionItemList')
+router.register(r'safetyindicators', SafetyIndicatorsViewSet, basename='safetyindicators')
 
 urlpatterns = [
     path('', include(router.urls)),
