f37886355bdf51cc9904b265a0d93573cc4aa0ac
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-03-18 11:10:39 +0900

    Update views.py

--- backend/repairingCost/views.py
+++ backend/repairingCost/views.py
@@ -12,6 +12,13 @@ class Pm02ViewSet(viewsets.ModelViewSet):
     queryset = Pm02.objects.all()
     serializer_class = Pm02Serializer
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        company_code = self.request.query_params.get('companyCode', None)
+        if company_code is not None:
+            queryset = queryset.filter(companyCode__code=company_code)
+        return queryset
+
     def list(self, request):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
@@ -27,43 +34,65 @@ class Pm03ViewSet(viewsets.ModelViewSet):
     queryset = Pm03.objects.all()
     serializer_class = Pm03Serializer
 
-    def list(self, request:Request):
-        pm03 = Pm03.objects.all()
-        serializer = Pm03Serializer(pm03, many=True)
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        company_code = self.request.query_params.get('companyCode', None)
+        if company_code is not None:
+            queryset = queryset.filter(companyCode__code=company_code)
+        return queryset
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
     
     def retrieve(self, request, pk=None):
-        queryset = Pm03.objects.all()
-        pm03 = get_object_or_404(queryset, pk=pk)
-        serializer = Pm03Serializer(pm03)
+        pm02 = get_object_or_404(self.get_queryset(), pk=pk)
+        serializer = self.get_serializer(pm02)
         return Response(serializer.data)
-    
+
+
+
 class Pm04ViewSet(viewsets.ModelViewSet):
     queryset = Pm04.objects.all()
     serializer_class = Pm04Serializer
 
-    def list(self, request:Request):
-        pm04 = Pm04.objects.all()
-        serializer = Pm04Serializer(pm04, many=True)
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        company_code = self.request.query_params.get('companyCode', None)
+        if company_code is not None:
+            queryset = queryset.filter(companyCode__code=company_code)
+        return queryset
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
     
     def retrieve(self, request, pk=None):
-        queryset = Pm04.objects.all()
-        pm04 = get_object_or_404(queryset, pk=pk)
-        serializer = Pm04Serializer(pm04)
+        pm02 = get_object_or_404(self.get_queryset(), pk=pk)
+        serializer = self.get_serializer(pm02)
         return Response(serializer.data)
+
+
 
 class Pm05ViewSet(viewsets.ModelViewSet):
     queryset = Pm05.objects.all()
     serializer_class = Pm05Serializer
 
-    def list(self, request:Request):
-        pm05 = Pm05.objects.all()
-        serializer = Pm05Serializer(pm05, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        company_code = self.request.query_params.get('companyCode', None)
+        if company_code is not None:
+            queryset = queryset.filter(companyCode__code=company_code)
+        return queryset
 
-def retrieve(self, request, pk=None):
-        queryset = Pm05.objects.all()
-        pm05 = get_object_or_404(queryset, pk=pk)
-        serializer = Pm05Serializer(pm05)
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        pm02 = get_object_or_404(self.get_queryset(), pk=pk)
+        serializer = self.get_serializer(pm02)
         return Response(serializer.data)
