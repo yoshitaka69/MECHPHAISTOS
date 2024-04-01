8ec84d1a506b4869b288f4e4cc9336a48c0341b3
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-04-01 15:11:41 +0900

    Update urls.py

--- backend/spareParts/urls.py
+++ backend/spareParts/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SparePartsViewSet,CompanyCodeSPViewSet
+from .views import SparePartsViewSet,CompanyCodeSPViewSet,BomcodeViewSet,CompanyBomCodeViewSet
 
 
 router = DefaultRouter()
 router.register(r'spareParts', SparePartsViewSet, basename='spareParts')
 router.register(r'sparePartsByCompany', CompanyCodeSPViewSet, basename='companyCode-spareParts')
+
+router.register(r'bomCode', BomCodeViewSet, basename='bomCode')
+router.register(r'bomCodeByCompany', CompanyBomCodeViewSet, basename='companyCode-bomCode')
 
 
 urlpatterns = [
