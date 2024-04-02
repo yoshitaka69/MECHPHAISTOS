68ff4d148cca45df907910ad49d5fc6a91f4b3d9
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-04-02 15:52:02 +0900

    Update urls.py

--- backend/taskList/urls.py
+++ backend/taskList/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (TaskListPM02ViewSet,CompanyCodeTaskListPM02ViewSet,
-                    TaskListPM03ViewSet,CompanyCodeTaskListPM03ViewSet,
-                    TaskListPM04ViewSet,CompanyCodeTaskListPM04ViewSet,
-                    TaskListPM05ViewSet,CompanyCodeTaskListPM05ViewSet,
+from .views import (TaskListPPM02ViewSet,CompanyCodeTaskListPPM02ViewSet,
+                    TaskListAPM02ViewSet,CompanyCodeTaskListAPM02ViewSet,
+
+                    TaskListPPM03ViewSet,CompanyCodeTaskListPPM03ViewSet,
+                    TaskListAPM03ViewSet,CompanyCodeTaskListAPM03ViewSet,
+
+                    TaskListAPM04ViewSet,CompanyCodeTaskListAPM04ViewSet,
+
+                    TaskListPPM05ViewSet,CompanyCodeTaskListPPM05ViewSet,
+                    TaskListAPM05ViewSet,CompanyCodeTaskListAPM05ViewSet,
+
                     TypicalTaskListViewSet,CompanyCodeTypicalTaskListViewSet,
                     TaskListViewSet,CompanyCodeTaskListViewSet)
 
@@ -16,14 +23,23 @@ router.register(r'typicalTaskListByCompany', CompanyCodeTypicalTaskListViewSet, 
 router.register(r'taskList', TaskListViewSet, basename='taskList')
 router.register(r'taskListByCompany', CompanyCodeTaskListViewSet, basename='companyCode-taskList')
 
-router.register(r'taskListPM02', TaskListPM02ViewSet, basename='taskListPM02')
-router.register(r'taskListPM02ByCompany', CompanyCodeTaskListPM02ViewSet, basename='companyCode-taskListPM02')
-router.register(r'taskListPM03', TaskListPM03ViewSet, basename='taskListPM03')
-router.register(r'taskListPM03ByCompany', CompanyCodeTaskListPM03ViewSet, basename='companyCode-taskListPM03')
-router.register(r'taskListPM04', TaskListPM04ViewSet, basename='taskListPM04')
-router.register(r'taskListPM04ByCompany', CompanyCodeTaskListPM04ViewSet, basename='companyCode-taskListPM04')
-router.register(r'taskListPM05', TaskListPM05ViewSet, basename='taskListPM05')
-router.register(r'taskListPM05ByCompany', CompanyCodeTaskListPM05ViewSet, basename='companyCode-taskListPM05')
+router.register(r'taskListPPM02', TaskListPPM02ViewSet, basename='taskListPPM02')
+router.register(r'taskListPPM02ByCompany', CompanyCodeTaskListPPM02ViewSet, basename='companyCode-taskListPPM02')
+router.register(r'taskListAPM02', TaskListAPM02ViewSet, basename='taskListAPM02')
+router.register(r'taskListAPM02ByCompany', CompanyCodeTaskListAPM02ViewSet, basename='companyCode-taskListAPM02')
+
+router.register(r'taskListPPM03', TaskListPPM03ViewSet, basename='taskListPPM03')
+router.register(r'taskListPPM03ByCompany', CompanyCodeTaskListPPM03ViewSet, basename='companyCode-taskListPPM03')
+router.register(r'taskListAPM03', TaskListAPM03ViewSet, basename='taskListAPM03')
+router.register(r'taskListAPM03ByCompany', CompanyCodeTaskListAPM03ViewSet, basename='companyCode-taskListAPM03')
+
+router.register(r'taskListPPM04', TaskListPPM04ViewSet, basename='taskListPPM04')
+router.register(r'taskListPPM04ByCompany', CompanyCodeTaskListPPM04ViewSet, basename='companyCode-taskListPPM04')
+
+router.register(r'taskListPPM05', TaskListPPM05ViewSet, basename='taskListPPM05')
+router.register(r'taskListPPM05ByCompany', CompanyCodeTaskListPPM05ViewSet, basename='companyCode-taskListPPM05')
+router.register(r'taskListAPM05', TaskListAPM05ViewSet, basename='taskListAPM05')
+router.register(r'taskListAPM05ByCompany', CompanyCodeTaskListAPM05ViewSet, basename='companyCode-taskListAPM05')
 
 urlpatterns = [
     path('task/', include(router.urls)),
