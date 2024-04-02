c0b5f1df97f03aec0ceb74818302934e172b8f7f
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-04-02 16:19:02 +0900

    Update signals.py

--- backend/calculation/signals.py
+++ backend/calculation/signals.py
@@ -1,9 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import CalTablePlannedPM02
-from taskList.models import TaskListPM02
+from taskList.models import TaskListPPM02,TaskListAPM02,TaskListPPM03,TaskListAPM03,TaskListAPM04,TaskListPPM05,TaskListAPM05,
 
-@receiver(post_save, sender=TaskListPM02)
+@receiver(post_save, sender=TaskListPPM02)
 def update_or_create_cal_table(sender, instance, **kwargs):
     print("update_or_create_cal_table シグナルがトリガーされました")
 
@@ -11,22 +11,22 @@ def update_or_create_cal_table(sender, instance, **kwargs):
     cal_table, created = CalTablePlannedPM02.objects.get_or_create(
         companyCode=instance.companyCode,
         defaults={
-            'no1HighCost': instance.laborCostOfPM02,
+            'no1HighCost': instance.laborCostOfPPM02,
             'no1HighCostTask': instance.taskName,
             
-            'no2HighCost': instance.laborCostOfPM02,
+            'no2HighCost': instance.laborCostOfPPM02,
             'no2HighCostTask': instance.taskName,
 
-            'no3HighCost': instance.laborCostOfPM02,
+            'no3HighCost': instance.laborCostOfPPM02,
             'no3HighCostTask': instance.taskName,
 
-            'no4HighCost': instance.laborCostOfPM02,
+            'no4HighCost': instance.laborCostOfPPM02,
             'no4HighCostTask': instance.taskName,
 
-            'no5HighCost': instance.laborCostOfPM02,
+            'no5HighCost': instance.laborCostOfPPM02,
             'no5HighCostTask': instance.taskName,
 
-            'no1LowCost' : instance.laborCostOfPM02,
+            'no1LowCost' : instance.laborCostOfPPM02,
             'no1LowCostTask' : instance.taskName,
 
             
@@ -38,32 +38,32 @@ def update_or_create_cal_table(sender, instance, **kwargs):
     else:
         print(f"既存の CalTablePlannedPM02 のレコードが見つかりました: {cal_table}")
         # レコードが既に存在する場合は、必要に応じて値を更新
-    if instance.laborCostOfPM02 > cal_table.no1HighCost:
-        cal_table.no1HighCost = instance.laborCostOfPM02
+    if instance.laborCostOfPPM02 > cal_table.no1HighCost:
+        cal_table.no1HighCost = instance.laborCostOfPPM02
         cal_table.no1HighCostTask = instance.taskName
             
     # no2HighCost, no3HighCost...等の同様のロジックを実装
-        cal_table.no2HighCost = instance.laborCostOfPM02
+        cal_table.no2HighCost = instance.laborCostOfPPM02
         cal_table.no2HighCostTask = instance.taskName
 
-        cal_table.no3HighCost = instance.laborCostOfPM02
+        cal_table.no3HighCost = instance.laborCostOfPPM02
         cal_table.no3HighCostTask = instance.taskName
 
-        cal_table.no4HighCost = instance.laborCostOfPM02
+        cal_table.no4HighCost = instance.laborCostOfPPM02
         cal_table.no4HighCostTask = instance.taskName
 
-        cal_table.no5HighCost = instance.laborCostOfPM02
+        cal_table.no5HighCost = instance.laborCostOfPPM02
         cal_table.no5HighCostTask = instance.taskName
         
     # 最低コストの更新
-    if instance.laborCostOfPM02 < cal_table.no1LowCost or cal_table.no1LowCost == 0:
-        cal_table.no1LowCost = instance.laborCostOfPM02
+    if instance.laborCostOfPPM02 < cal_table.no1LowCost or cal_table.no1LowCost == 0:
+        cal_table.no1LowCost = instance.laborCostOfPPM02
         cal_table.no1LowCostTask = instance.taskName
 
     # 指定されたcompanyCodeに対するlaborCostOfPM02の平均値を計算
-    average_cost = TaskListPM02.objects.filter(
+    average_cost = TaskListPPM02.objects.filter(
         companyCode=instance.companyCode
-    ).aggregate(Avg('laborCostOfPM02'))['laborCostOfPM02__avg']
+    ).aggregate(Avg('laborCostOfPPM02'))['laborCostOfPPM02__avg']
 
     # averageCostの更新
     if average_cost is not None:
