6ad28ffa02a3608f097666a9e2e580c91242755b
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-04-03 08:16:00 +0900

    Update models.py

--- backend/nearMiss/models.py
+++ backend/nearMiss/models.py
@@ -36,6 +36,43 @@ class NearMiss(models.Model):
     def __str__(self):
             return f'{self.nearMissNo}'
 
+    #nerMiss総数をカウントメソッド
+    def total_of_near_miss(self):
+        return NearMiss.objects.filter(companyCode=self.companyCode).count()
+
+    #Risk Level A の割合
+    def rate_of_level_a(self):
+        total_near_miss = self.total_of_near_miss()
+        if total_near_miss > 0:
+            level_a_count = NearMiss.objects.filter(companyCode=self.companyCode, measures__contains='A').count()
+            return (level_a_count / total_near_miss) * 100
+        else:
+            return 0
+
+    #対策Actionの有無
+    def rate_of_action_items(self):
+        total_action_items = self.ActionItems.count()
+        solved_action_items = self.solvedActionItems.count()
+        if total_action_items > 0:
+            return (solved_action_items / total_action_items) * 100
+        else:
+            return 0
+
+    #Safety indicatorsの設定
+    def calculate_safety_indicators(self):
+        rate_of_level_a = self.rate_of_level_a()
+        rate_of_action_items = self.rate_of_action_items()
+        if rate_of_level_a >= 40:
+            return 'High'
+        elif rate_of_level_a < 40:
+            if rate_of_action_items > 80:
+                return 'High'
+            elif 40 < rate_of_action_items <= 80:
+                return 'Middle'
+            elif 0 < rate_of_action_items <= 40:
+                return 'Low'
+        return 'Undefined'
+
 
 
 
