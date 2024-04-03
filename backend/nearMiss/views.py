0e36e2ac82554bb4181bf0c8eb17be21bd4211c9
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-04-03 10:34:43 +0900

    Update views.py

--- backend/nearMiss/views.py
+++ backend/nearMiss/views.py
@@ -10,49 +10,28 @@ from .models import NearMiss, CompanyCode, ActionItemList, SafetyIndicators
 from .serializers import NearMissSerializer, CompanyNearMissSerializer, ActionItemListSerializer, SafetyIndicatorsSerializer
 
 
+
+
 class NearMissViewSet(viewsets.ModelViewSet):
     queryset = NearMiss.objects.all()
     serializer_class = NearMissSerializer
     
-    def post(self, request, *args, **kwargs):
-        for company_data in request.data:
-            company_code = company_data.get('companyCode')
-            near_misses = company_data.get('nearMissList', [])
-
-            # 会社コードの処理
-            company, created = CompanyCode.objects.get_or_create(companyCode=company_code)
-
-            for near_miss_data in near_misses:
-                user_data = near_miss_data.pop('userName', {})
-                user_name = user_data.get('userName')
-
-                # ユーザーデータの処理
-                user, created = CustomUser.objects.get_or_create(userName=user_name)
-
-                # ニアミスレポートの処理
-                near_miss_data['userName'] = user
-                near_miss, created = NearMiss.objects.update_or_create(
-                    nearMissNo=near_miss_data['nearMissNo'],
-                    defaults=near_miss_data
-                )
-                # 会社にニアミスレポートをリンクする
-                near_miss.companyCode = company
-                near_miss.save()
-
-        return Response({"message": "データが正常に処理されました"}, status=status.HTTP_200_OK)
-
-
-
-
-# views.py
 class CompanyNearMissViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = CompanyNearMissSerializer
 
-    def get_queryset(self):
-        company_code = self.request.query_params.get('companyCode', None)
-        if company_code is not None:
-            return CompanyCode.objects.filter(companyCode=company_code).prefetch_related('nearMiss_companyCode')
-        return CompanyCode.objects.all()
+    def post(self, request, *args, **kwargs):
+        serializer = CompanyNearMissSerializer(data=request.data)
+
+        if serializer.is_valid():
+            # NearMiss のデータを保存
+            near_miss_instance = serializer.save()
+
+            # companyCode を取得
+            company_code = near_miss_instance.companyCode
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 
