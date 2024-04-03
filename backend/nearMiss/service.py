24a9f3583fb62e7c99aa86b660c5a8662575f517
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-04-03 11:13:28 +0900

    Update service.py

--- backend/nearMiss/service.py
+++ backend/nearMiss/service.py
@@ -1,7 +1,7 @@
-from .models import SafetyIndicator, NearMiss
+from .models import SafetyIndicators, NearMiss
 
 def update_safety_indicator(company_code):
-    indicator, created = SafetyIndicator.objects.get_or_create(companyCode=company_code)
+    indicator, created = SafetyIndicators.objects.get_or_create(companyCode=company_code)
     total_near_miss_count = NearMiss.objects.filter(companyCode=company_code).count()
     indicator.totalOfNearMiss = total_near_miss_count
     indicator.save()
