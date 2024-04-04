b5400c13d132053a95db817a0d341dba828bca55
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-04-04 14:04:56 +0900

    Update urls.py

--- backend/junctionTable/urls.py
+++ backend/junctionTable/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MasterDataTableViewSet,CompanyCodeMDTViewSet
+from .views import (MasterDataTableViewSet,CompanyCodeMDTViewSet,
+                    BomAndTaskSerializer,CompanyCodeBomAndTaskSerializer,
+                    AlertScheduleSerializer,CompanyCodeAlertScheduleViewSet)
 
 
 router = DefaultRouter()
 router.register(r'masterDataTable', MasterDataTableViewSet, basename='masterDataTable')
 router.register(r'masterDataTableByCompany', CompanyCodeMDTViewSet, basename='companyCode-masterDataTable')
 
+router.register(r'bomAndTask', BomAndTaskViewSet, basename='bomAndTask')
+router.register(r'bomAndTaskByCompany', CompanyCodeBomAndTaskViewSet, basename='companyCode-bomAndTask')
+
+router.register(r'alertSchedule', AlertSchedulekViewSet, basename='alertSchedule')
+router.register(r'alertScheduleByCompany', CompanyCodeAlertScheduleViewSet, basename='companyCode-alertSchedule')
 
 urlpatterns = [
     path('junctionTable/', include(router.urls)),
