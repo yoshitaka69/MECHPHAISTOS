c0673452e3ae64e9db52a34884763fc606295305
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-04-04 13:41:54 +0900

    Update views.py

--- backend/junctionTable/views.py
+++ backend/junctionTable/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from .models import MasterDataTable,BomToTask,AlertSchedule
+from .models import MasterDataTable,BomAndTask,AlertSchedule
 from accounts.models import CompanyCode
 from .serializers import  (MasterDataTableSerializer, CompanyCodeMDTSerializer,
-                        BomToTaskSerializer,CompanyCodeBomToTaskSerializer,
+                        BomAndTaskSerializer,CompanyCodeBomToTaskSerializer,
                         AlertScheduleSerializer,CompanyCodeAlertScheduleSerializer)
 
 
@@ -23,25 +23,27 @@ class CompanyCodeMDTViewSet(viewsets.ModelViewSet):
 
 
 #BomAndTask
-class MasterDataTableViewSet(viewsets.ModelViewSet):
-    queryset = MasterDataTable.objects.all()
-    serializer_class = MasterDataTableSerializer
+class BomAndTaskViewSet(viewsets.ModelViewSet):
+    queryset = BomAndTask.objects.all()
+    serializer_class = BomAndTaskSerializer
 
 
-class CompanyCodeMDTViewSet(viewsets.ModelViewSet):
-    serializer_class = CompanyCodeMDTSerializer
-
-    def get_queryset(self):
-        return CompanyCode.objects.prefetch_related('masterDataTable_companyCode').all()#ここでのrelatedNameの間違いは要注意
-
-
-class MasterDataTableViewSet(viewsets.ModelViewSet):
-    queryset = MasterDataTable.objects.all()
-    serializer_class = MasterDataTableSerializer
-
-
-class CompanyCodeMDTViewSet(viewsets.ModelViewSet):
-    serializer_class = CompanyCodeMDTSerializer
+class CompanyCodeBomAndTaskViewSet(viewsets.ModelViewSet):
+    serializer_class = CompanyCodeBomAndTaskSerializer
 
     def get_queryset(self):
-        return CompanyCode.objects.prefetch_related('masterDataTable_companyCode').all()#ここでのrelatedNameの間違いは要注意
+        return CompanyCode.objects.prefetch_related('bomAndTask_companyCode').all()#ここでのrelatedNameの間違いは要注意
+
+
+
+#AlertSchedule
+class AlertScheduleViewSet(viewsets.ModelViewSet):
+    queryset = AlertSchedule.objects.all()
+    serializer_class = AlertScheduleSerializer
+
+
+class CompanyCodeAlertScheduleiewSet(viewsets.ModelViewSet):
+    serializer_class = CompanyCodeAlertScheduleSerializer
+
+    def get_queryset(self):
+        return CompanyCode.objects.prefetch_related('alertSchedule_companyCode').all()#ここでのrelatedNameの間違いは要注意
