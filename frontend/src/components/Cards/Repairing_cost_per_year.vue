310d165da730bb59fc26a7fb0eae2d7666c491ba
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-04-08 08:51:51 +0900

    Update Repairing_cost_per_year.vue

--- frontend/src/components/Cards/Repairing_cost_per_year.vue
+++ frontend/src/components/Cards/Repairing_cost_per_year.vue
@@ -4,7 +4,7 @@
       <div class="flex justify-content-between mb-3">
         <div>
           <span class="block text-500 font-medium mb-3">Number of near misses</span>
-          <div class="text-900 font-medium text-xl">{{ nearMissTotal }}</div>
+          <div class="text-900 font-medium text-xl">{{ totalActualCost }}</div>
         </div>
         <div class="flex align-items-center justify-content-center bg-orange-100 border-round"
           style="width: 2.5rem; height: 2.5rem">
@@ -23,33 +23,28 @@ import axios from 'axios';
 import { ref, onMounted } from 'vue';
 import { useUserStore } from '@/stores/userStore'; // Piniaストアをインポート
 
-
 export default {
   setup() {
-    const nearMissTotal = ref(0);
+    const totalActualCost = ref('0.00'); // 初期値を設定
     const userStore = useUserStore();
 
-    const fetchNearMissData = async () => {
+    const fetchTotalActualCost = async () => {
       try {
-        console.log("Fetching data with companyCode:", userStore.companyCode); // リクエストのログ
-        const response = await axios.get(`http://127.0.0.1:8000/api/nearMiss/safetyIndicatorsByCompany/`, {
+        const response = await axios.get('http://127.0.0.1:8000/api/nearMiss/trendSafetyIndicatorsByCompany/', {
           params: { companyCode: userStore.companyCode }
         });
-        console.log("Response data:", response.data); // レスポンスのログ
-        const data = response.data.find(d => d.companyCode === userStore.companyCode);
-        if (data && data.safetyIndicatorsList.length > 0) {
-          nearMissTotal.value = data.safetyIndicatorsList[0].totalOfNearMiss;
-        } else {
-          console.log("No matching data found for companyCode:", userStore.companyCode);
+        const data = response.data;
+        if (data && data.summedActualCostList && data.summedActualCostList.length > 0) {
+          totalActualCost.value = data.summedActualCostList[0].totalActualCost; // totalActualCostの設定
         }
       } catch (error) {
-        console.error('Error fetching data:', error); // エラーのログ
+        console.error('Error fetching total actual cost:', error);
       }
     };
 
-    onMounted(fetchNearMissData);
+    onMounted(fetchTotalActualCost);
 
-    return { nearMissTotal };
+    return { totalActualCost };
   },
 };
 </script>
