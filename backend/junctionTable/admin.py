a1d6ef6ae3dde96b75c263e7fa6f86def9df4c79
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-04-11 08:26:42 +0900

    Update admin.py

--- backend/junctionTable/admin.py
+++ backend/junctionTable/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MasterDataTable,BomAndTask
+from .models import MasterDataTable,BomAndTask,CeListAndTask
 
 class MasterDataTableAdmin(admin.ModelAdmin):
 
