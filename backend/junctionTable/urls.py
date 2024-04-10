634eccb96a9680736a1ca7558d368b12d6977162
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-04-11 07:49:32 +0900

    Update urls.py

--- backend/junctionTable/urls.py
+++ backend/junctionTable/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (MasterDataTableViewSet,CompanyCodeMDTViewSet,
-                    BomAndTaskViewSet,CompanyCodeBomAndTaskViewSet)
+                    BomAndTaskViewSet,CompanyCodeBomAndTaskViewSet,CeListAndTaskViewSet,CompanyCodeCeListAndTaskViewSet,)
 
 
 router = DefaultRouter()
@@ -10,6 +10,9 @@ router.register(r'masterDataTableByCompany', CompanyCodeMDTViewSet, basename='co
 
 router.register(r'bomAndTask', BomAndTaskViewSet, basename='bomAndTask')
 router.register(r'bomAndTaskByCompany', CompanyCodeBomAndTaskViewSet, basename='companyCode-bomAndTask')
+
+router.register(r'ceListAndTask', CeListAndTaskViewSet, basename='ceListAndTask')
+router.register(r'ceListAndTaskByCompany', CompanyCodeCeListAndTaskViewSet, basename='companyCode-ceListAndTask')
 
 urlpatterns = [
     path('junctionTable/', include(router.urls)),
