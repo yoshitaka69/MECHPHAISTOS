78d0732df0761d69385c0ef384e10f1811afeb3a
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-04-16 14:44:38 +0900

    Update admin.py

--- backend/nearMiss/admin.py
+++ backend/nearMiss/admin.py
@@ -4,7 +4,7 @@ from .models import NearMiss,SafetyIndicators,TrendSafetyIndicators
 
 class NearMissAdmin(admin.ModelAdmin):
     list_display = ('companyCode','userName', 'nearMissNo', 'department','dateOfOccurrence','placeOfOccurrence','typeOfAccident','description','factor','injuredLv','equipmentDamageLv','affectOfEnviroment','newsCoverage','measures','actionItems','solvedActionItems','createdDay','updateDay')
-    readonly_fields = ('nearMissNo','measures') 
+    readonly_fields = ('nearMissNo','measures','createdDay','updateDay') 
     search_fields = ('companyCode','userName', 'nearMissNo', 'department','dateOfOccurrence','placeOfOccurrence','typeOfAccident','description','factor','injuredLv','equipmentDamageLv','affectOfEnviroment','newsCoverage','measures','actionItems','solvedActionItems','createdDay','updateDay')
     list_filter = ('companyCode',"userName",) # adminで右側にあるフィルターBOXのこと
     ordering = ('companyCode',) # 表示する順番
@@ -17,6 +17,7 @@ class NearMissAdmin(admin.ModelAdmin):
 
 class SafetyIndicatorsAdmin(admin.ModelAdmin):
     list_display = ('companyCode', 'companyName', 'safetyIndicators', 'dangerArea','totalOfNearMiss', 'countOfLevelA', 'rateOflevelA', 'countOfActionItems', 'countOfSolvedActionItems', 'rateOfActionItems')
+    readonly_fields = ('safetyIndicators','totalOfNearMiss', 'countOfLevelA', 'rateOflevelA', 'countOfActionItems', 'countOfSolvedActionItems', 'rateOfActionItems') 
     search_fields = ('companyCode', 'companyName', 'safetyIndicators', 'dangerArea','totalOfNearMiss', 'countOfLevelA', 'rateOflevelA', 'countOfActionItems', 'countOfSolvedActionItems', 'rateOfActionItems')
     list_filter = ('companyCode','companyName',) # adminで右側にあるフィルターBOXのこと
     ordering = ('companyCode',) # 表示する順番
@@ -28,6 +29,7 @@ class SafetyIndicatorsAdmin(admin.ModelAdmin):
 
 class TrendSafetyIndicatorsAdmin(admin.ModelAdmin):
     list_display = ('companyCode', 'companyName', 'safetyIndicators', 'lastUpdateDay')
+    readonly_fields = ( 'safetyIndicators', 'lastUpdateDay') 
     search_fields = ('companyCode', 'companyName', 'safetyIndicators', 'lastUpdateDay')
     list_filter = ('companyCode','companyName',) # adminで右側にあるフィルターBOXのこと
     ordering = ('companyCode',) # 表示する順番
