1b9c276237dbf632a96bfcda999ed0b8a93546b5
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-04-25 07:41:28 +0900

    Update models.py

--- backend/workOrder/models.py
+++ backend/workOrder/models.py
@@ -1,2 +1,22 @@
 from django.db import models
 
+
+class WorkOrder(models.Model):
+  companyCode
+  plant
+  equipment
+  workOrderNo
+  description
+  situation
+  raiseHands
+
+
+
+class WorkPermission(models.Model)
+  companyCode
+  plant
+  equipment
+
+
+class ForSafetyWork(models.Model)
+  
