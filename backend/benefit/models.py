06f39fa90d87e7e4baf7f23af3a049c13a64ebdd
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-06-07 07:32:39 +0900

    Update models.py

--- backend/benefit/models.py
+++ backend/benefit/models.py
@@ -5,9 +5,6 @@ from django.db import models
 class SimulationBenefit(models.Model):
   
   companyCode = models.ForeignKey(CompanyCode, on_delete=models.CASCADE, related_name='equipment_companyCode',null=True, blank=True)
-  companyName = models.ForeignKey(CompanyName, on_delete=models.CASCADE, related_name='equipment_companyName', null=True, blank=True)
-  plant = models.ForeignKey(Plant, on_delete=models.CASCADE, related_name='equipment_plant',null=True, blank=True)
-  equipment = models.CharField(verbose_name='equipment', max_length=200,null=True,blank=True)
   
   class Meta:
     verbose_name = 'Equipment'
@@ -22,11 +19,38 @@ class ImprovementBenefit(models.Model):
 
   companyCode = models.ForeignKey(CompanyCode, on_delete=models.CASCADE, related_name='equipment_companyCode',null=True, blank=True)
 
+  class Meta:
+    verbose_name = 'Equipment'
+    verbose_name_plural = 'Equipment'
+    ordering = ('companyCode',) 
+    
+    def __str__(self):
+      return f'{self.equipment}'
+
+
 
 
 class RiskAvoidBenefit(models.Model):
   companyCode = models.ForeignKey(CompanyCode, on_delete=models.CASCADE, related_name='equipment_companyCode',null=True, blank=True)
 
+  class Meta:
+    verbose_name = 'Equipment'
+    verbose_name_plural = 'Equipment'
+    ordering = ('companyCode',) 
+    
+    def __str__(self):
+      return f'{self.equipment}'
+
+
+
 
 class PartsManagementBenefit(models.Model):
+
+    class Meta:
+    verbose_name = 'Equipment'
+    verbose_name_plural = 'Equipment'
+    ordering = ('companyCode',) 
+    
+    def __str__(self):
+      return f'{self.equipment}'
   
