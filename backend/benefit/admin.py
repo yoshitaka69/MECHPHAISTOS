5b1f907a9da0463892d382104bb18df49c6a00ef
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-06-07 09:24:45 +0900

    Update admin.py

--- backend/benefit/admin.py
+++ backend/benefit/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import CalTablePlannedPM02,CalTableActualPM02,
 
-# Register your models here.
+
+
+
+class CalTablePlannedPM02Admin(admin.ModelAdmin):
+    list_display = ('companyCode','companyName','plant','no1HighCost','no2HighCost','no3HighCost','no4HighCost','no5HighCost','no1LowCost','averageCost',)
+    readonly_fields = ('no1HighCost','no2HighCost','no3HighCost','no4HighCost','no5HighCost','no1LowCost','averageCost',) 
+    search_fields = ('companyCode','companyName','plant','no1HighCost','no2HighCost','no3HighCost','no4HighCost','no5HighCost','no1LowCost','averageCost',)
+    list_filter = ('companyCode','companyName','plant','no1HighCost','no2HighCost','no3HighCost','no4HighCost','no5HighCost','no1LowCost','averageCost',)
+    ordering = ('plant',)
+    save_on_top = True
+
+    list_per_page = 50 
+
+
+admin.site.register(SummedPlannedCost,SummedPlannedCostAdmin)
+admin.site.register(SummedActualCost,SummedActualCostAdmin)
