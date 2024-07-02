6538c0698ff907ee09fc4096f2be5c46b53f8b66
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-07-02 10:57:14 +0900

    Update views.py

--- backend/benefit/views.py
+++ backend/benefit/views.py
@@ -1,3 +1,75 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+#PlanOptimization
+class POBViewSet(viewsets.ModelViewSet):
+    queryset = PlanOptimizationBenefit.objects.all()
+    serializer_class = POBSerializer
+
+class CompanyCodePOBViewSet(viewsets.ModelViewSet):
+    serializer_class = CompanyCodePOBSerializer
+
+#クエリパラメータでのフィルターリング
+    def get_queryset(self):
+        queryset = CompanyCode.objects.prefetch_related('planOptimizationBenefit_companyCode').all()
+        company_code = self.request.query_params.get('companyCode', None)
+        if company_code:
+            queryset = queryset.filter(companyCode=company_code)
+        return queryset
+
+
+
+#ImprovementBenefit
+class IBViewSet(viewsets.ModelViewSet):
+    queryset = ImprovementBenefit.objects.all()
+    serializer_class = IBSerializer
+
+class CompanyCodeIBViewSet(viewsets.ModelViewSet):
+    serializer_class = CompanyCodeIBSerializer
+
+#クエリパラメータでのフィルターリング
+    def get_queryset(self):
+        queryset = CompanyCode.objects.prefetch_related('improvementBenefit_companyCode').all()
+        company_code = self.request.query_params.get('companyCode', None)
+        if company_code:
+            queryset = queryset.filter(companyCode=company_code)
+        return queryset
+
+
+
+
+#riskAvoidanceBenefit
+class RABViewSet(viewsets.ModelViewSet):
+    queryset = RiskAvoidanceBenefit.objects.all()
+    serializer_class = RABSerializer
+
+class CompanyCodeRABViewSet(viewsets.ModelViewSet):
+    serializer_class = CompanyCodeRABSerializer
+
+#クエリパラメータでのフィルターリング
+    def get_queryset(self):
+        queryset = CompanyCode.objects.prefetch_related('riskAvoidanceBenefit_companyCode').all()
+        company_code = self.request.query_params.get('companyCode', None)
+        if company_code:
+            queryset = queryset.filter(companyCode=company_code)
+        return queryset
+
+
+
+#vendorSelectionBenefit
+class VSBViewSet(viewsets.ModelViewSet):
+    queryset = VendorSelectionBenefit.objects.all()
+    serializer_class = VSBSerializer
+
+class CompanyCodeVSBViewSet(viewsets.ModelViewSet):
+    serializer_class = CompanyCodeVSBSerializer
+
+#クエリパラメータでのフィルターリング
+    def get_queryset(self):
+        queryset = CompanyCode.objects.prefetch_related('vendorSelectionBenefit_companyCode').all()
+        company_code = self.request.query_params.get('companyCode', None)
+        if company_code:
+            queryset = queryset.filter(companyCode=company_code)
+        return queryset
+
+
