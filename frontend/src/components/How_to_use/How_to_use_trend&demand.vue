f1f9ec3b33d2ea84c5150a24ff2db9aaeb10fcbc
YOSHITAKA NOTO <133880120+yoshitaka69@users.noreply.github.com>
2024-09-18 13:29:13 +0900

    Update How_to_use_trend&demand.vue

--- frontend/src/components/How_to_use/How_to_use_trend&demand.vue
+++ frontend/src/components/How_to_use/How_to_use_trend&demand.vue
@@ -43,12 +43,7 @@
                         </div>
                         <div class="line-height-3 text-xl text-900 mb-5">Metus aliquam eleifend mi in nulla posuere. In vitae turpis massa sed elementum tempus egestas sed. Dui ut ornare lectus sit. Massa ultricies mi quis hendrerit.</div>
                         <div class="line-height-3 text-lg text-900 mb-4">
-                            Here, we will explain how to optimize repair costs. These days, there is a strong demand to reduce repair costs, but repair costs should not be reduced easily (I myself was inspired to create MECHPHAISTOS because of such
-                            requests). However, unfortunately, if you ask whether maintenance is being performed at an appropriate repair cost, I think that there are probably only a few plant maintenance workers. The reason for this is that they do
-                            not know what appropriate maintenance is, or they do not have the time to pursue such things in their daily work. In such a situation, they tend to perform excessive maintenance in order to avoid fear of production
-                            equipment troubles. This tool first visualizes repair costs. PM02 is preventive maintenance. PM03 is corrective maintenance. PM04 is breakdown trouble. PM05 is improvement activity. Of these, PM02, PM03, and PM05 can be
-                            considered as maintenance that can be performed in a planned manner. Therefore, if you fill in the form below along with your budget application, you can visualize your future budget. Furthermore, this data will be
-                            important for creating future budgets and analyzing, so please record it in the form below.
+                            Here, w
                         </div>
                         <blockquote class="surface-50 line-height-3 text-xl text-800 mb-4 py-3 px-3 border-round mx-0">
                             “In short, GPU.js is a JavaScript acceleration library that can be used for general-purpose computations on GPUs using JavaScript. It supports browsers, Node.js and TypeScript.”
